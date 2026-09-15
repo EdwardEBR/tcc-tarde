38f1038d14cbe9652f01cacb71ec972cd91243b7
3dw <eduardopontes797@gmail.com>
2026-09-14 23:52:36 -0300

    Adicionado todo um menu
    Quase 70% dos botoes funcionando

---
+++ Assets/Scenes/SlimUI/Modern Menu 1/Scripts/Misc/ExtraLinks.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace SlimUI.ModernMenu{
+    public class ExtraLinks : MonoBehaviour{
+        public void diogo(){
+            Application.OpenURL("https://www.instagram.com/_.fernandesd?utm_source=ig_web_button_share_sheet&stkn=ZDNlZDc0MzIxNw==");
+        }
+
+        public void eduardo(){
+            Application.OpenURL("https://github.com/EdwardEBR");
+        }
+
+        public void felipe(){
+            Application.OpenURL("https://github.com/Felipe-Cyller");
+        }
+
+        public void gabriel(){
+            Application.OpenURL("https://github.com/Gabrielzed");
+        }
+        public void gaby(){
+            Application.OpenURL("https://www.instagram.com/wxsgab_/");
+        }
+    }
+}
